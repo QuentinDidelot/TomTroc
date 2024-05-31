f31bcd624b32e4a14ec8342bb1e0bab9ceff0482
Quentin D. <quentin.didelot13@gmail.com>
2024-05-31 14:54:48 +0200

    Début du projet
    - Construction du projet
    - Dossiers et sous-dossiers créés
    - Header fait
    - Première div faite
    - Début d'import des images qui serviront par la suite dans la BDD

---
+++ index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <script src="https://kit.fontawesome.com/d8c1f4a5b2.js" crossorigin="anonymous"></script>
+
+        <link rel="stylesheet" href="./css/style.css">
+        <title>TomTroc</title>
+    </head>
+
+    <body>
+        <div class="main_container">
+
+<!---------------------------- HEADER ---------------------------->
+
+            <header>
+                <nav>
+                    <div class="nav_left">
+                        <img class="logo" src="./img/icones/header.png" alt="Logo TomTroc (TT)">
+                        <h1> Tom Troc </h1> 
+                        <a href="">Accueil</a>
+                        <a href="">Nos livres à l'échange</a>
+                    </div>
+
+                    <div class="nav_right">
+                        <a href=""><i class="fa-regular fa-comments"></i> Messagerie</a>
+                        <a href=""><i class="fa-regular fa-user"></i> Mon compte</a>
+                        <a href="">Connexion</a>
+                    </div> 
+                </nav>
+
+            </header>
+
+            <div class="join_us">
+                <div class="join">
+                    <h2>Rejoignez nos lecteurs passionnés </h2>
+                    <p> Donnez une nouvelle vie à vos livres en les échangeant avec d'autres amoureux de la lecture. Nous croyons en la magie du partage de connaissances et d'histoires à travers les livres.</p>
+                    <a href="" class="button_type_1" > Découvrir </a>
+                </div>
+
+                <div class="join_picture">
+                    <img class="home_picture" src="./img/home_picture.jpg">
+                </div>
+            </div>
+
+            <div class="last_books">
+                <div class="books_card">
+                </div>
+
+                <div class="books_card">
+                </div>
+
+                <div class="books_card">
+                </div>
+
+                <div class="books_card">
+                </div>
+            </div>
+
+            <div class="how_it_works">
+                <div class="steps">
+                </div>
+            </div>
+
+            <div class="banner">
+            </div>
+
+            <div class="values">
+            </div>
+
+            <footer>
+
+            </footer>   
+        </div>
+    </body>
+</html>
