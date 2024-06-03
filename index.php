6f587e42a0e20571d4418ec647c4c5e185900428
Quentin D. <quentin.didelot13@gmail.com>
2024-06-03 12:53:31 +0200

    Page "home"
    - Bannière faite
    - Bloc "Nos Valeurs"
    - Footer

--- index.php
+++ index.php
@@ -94,18 +94,60 @@
             </section>
 
             <section class="how_it_works">
-                <div class="steps">
+                <h2> Comment ça marche ?</h2>
+                <p>Échanger des livres avec TomTroc c'est simple et 
+                    amusant ! Suivez ces étapes pour commencer : </p>
+                <div class="steps_container">
+                    <div class="step">
+                        <p>Inscrivez-vous gratuitement sur notre plateforme.</p>
+                    </div>
+
+                    <div class="step">
+                        <p>Ajoutez les livres que vous souhaitez échanger à votre profil.</p>
+                    </div>
+
+                    <div class="step">
+                        <p>Parcouez les livres disponibles chez d'autres membres.</p>
+                    </div>
+
+                    <div class="step">
+                        <p>Proposez un échange et discutez avec d'autres passionnés de lecture.</p>
+                    </div>
+                </div>
+
+                <div class="button_container">
+                    <a href="" class="button_type_2" > Voir tous les livres </a>
                 </div>
             </section>
 
             <section class="banner">
+                <img src="img/banner.jpg" alt="une bibliothèque">
             </section>
 
             <section class="values">
+                <div class="values_container">
+                    <h2>Nos valeurs</h2>
+                    <p>Chez Tom Troc, nous mettons l'accent sur le partage, la découverte et la communauté. Nos valeurs sont ancrées dans notre passion pour les livres et notre désir de créer des liens entre les lecteurs. Nous croyons en la puissance des histoires pour rassembler les gens et inspirer des conversations enrichissantes <br><br>
+
+                    Notre association a été fondée avec une conviction profonde : chaque livre mérite d'être lu et partagé. <br><br>
+
+                    Nous sommes passionnés par la création d'une plateforme conviviale qui permet aux lecteurs de se connecter, de partager leurs découvertes littéraires et d'échanger des livres qui attendent patiemment sur les étagères.  <br><br>
+                    </p>
+
+                    <div class="signature_container">                     
+                        <p class="signature">L'équipe Tom Troc </p>
+                        <img src="img/icones/heart.png" alt="un coeur vert">
+                    </div>
+                </div>             
             </section>
 
             <footer>
-
+                <div class="links_container">
+                    <a href="">Politique de confidentialité</a>
+                    <a href="">Mentions légales</a>
+                    <a href="">Tom Troc©</a>
+                    <img src="img/icones/footer_1.png" class="logo_footer" alt="logo du site en vert (TT)">
+                </div>
             </footer>   
         </div>
     </body>
