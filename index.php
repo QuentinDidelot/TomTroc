d547fc3c5531d9c8f3e90fe79e7bf4847b081dcc
Quentin D. <quentin.didelot13@gmail.com>
2024-06-03 10:50:16 +0200

    Section : Les derniers livres ajoutés
    - Cartes faites
    - Boutons modifiés (hover)
    - Importation des images dans le dossier "img" > "books"

--- index.php
+++ index.php
@@ -32,7 +32,7 @@
 
             </header>
 
-            <div class="join_us">
+            <section class="join_us">
                 <div class="join">
                     <h2>Rejoignez nos lecteurs passionnés </h2>
                     <p> Donnez une nouvelle vie à vos livres en les échangeant avec d'autres amoureux de la lecture. Nous croyons en la magie du partage de connaissances et d'histoires à travers les livres.</p>
@@ -43,32 +43,66 @@
                     <img class="home_picture" src="./img/home_picture.jpg" alt="homme entouré de livre, en pleine lecture">
                     <div class="caption">Hamza</div>
                 </div>
-            </div>
+            </section>
 
-            <div class="last_books">
-                <div class="books_card">
+            <section class="last_books">
+                <h2> Les derniers livres ajoutés </h2>
+
+                <div class="books_container">
+
+                    <a href="" class="last_books_link"> 
+                        <article class="books_card">
+                            <img src="img/books/le_trone_de_fer_1.jpg" alt="Le trone de fer, tome 1">
+                            <h3 class="book_title">Le trone de fer, tome 1</h3>
+                            <p class="author">George R.R. Martin</p>
+                            <p class="sold_by">Vendu par : .....</p>
+                        </article>
+                    </a>
+
+                    <a href="" class="last_books_link"> 
+                        <article class="books_card">
+                            <img src="img/books/le_silmarillion.jpg" alt="Le Silmarillion">
+                            <h3 class="book_title">Le Silmarillion</h3>
+                            <p class="author">J.R.R Tolkien</p>
+                            <p class="sold_by">Vendu par : .....</p>
+                        </article>
+                    </a>
+
+                    <a href="" class="last_books_link"> 
+                        <article class="books_card">
+                            <img src="img/books/harry_potter_1.jpg" alt="Harry Potter, tome 1">
+                            <h3 class="book_title">Harry Potter à l'école des sorciers</h3>
+                            <p class="author">J.K Rowling</p>
+                            <p class="sold_by">Vendu par : .....</p>
+                        </article>
+                    </a>
+
+                    <a href="" class="last_books_link"> 
+                        <article class="books_card">
+                            <img src="img/books/the_witcher_1.jpg" alt="The Witcher, tome 1">
+                            <h3 class="book_title">The Witcher, tome 1</h3>
+                            <p class="author">Andrzej Sapkowski</p>
+                            <p class="sold_by">Vendu par : .....</p>
+                        </article>
+                    </a>
+                </div>  
+
+                <div class="button_container">
+                    <a href="" class="button_type_1" > Voir tous les livres </a>
                 </div>
 
-                <div class="books_card">
-                </div>
+            </section>
 
-                <div class="books_card">
-                </div>
-
-                <div class="books_card">
-                </div>
-            </div>
-
-            <div class="how_it_works">
+            <section class="how_it_works">
                 <div class="steps">
                 </div>
-            </div>
+            </section>
 
-            <div class="banner">
-            </div>
+            <section class="banner">
+            </section>
 
-            <div class="values">
-            </div>
+            <section class="values">
+            </section>
 
             <footer>
 
