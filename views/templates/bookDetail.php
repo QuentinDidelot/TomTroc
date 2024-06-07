96c05ac6e99863939b63cae2c89dc847730656f2
Quentin D. <quentin.didelot13@gmail.com>
2024-06-07 17:38:35 +0200

    Page : bookDetail presque terminée

--- views/templates/bookDetail.php
+++ views/templates/bookDetail.php
@@ -8,18 +8,22 @@
             <div class="detail_container_right">  
                 <div class="detail_title"> 
                     <h1>Titre du livre</h1>
-                    <span class="detail_author">Par ......</span>
+                    <span class="detail_author">Par ......</span><br>
                 </div>
-                
+                <span class="deco_line">______</span> 
                 <div class="detail_book">
-                    <h3 class="detail_description_title">Description</h3>
+
+                    <h3 class="subtitle">Description</h3>
 
                     <div class="detail_description">  "Harry Potter et l'Ordre du Phénix", cinquième tome de la saga de J.K. Rowling, marque un tournant sombre et mature dans l'histoire. Harry, désormais adolescent, doit faire face à la montée en puissance de Voldemort, à l'injustice du Ministère de la Magie et aux défis émotionnels de son âge. Ce tome se distingue par son exploration des thèmes de la résistance et de la rébellion, incarnés par la création de l'Ordre du Phénix et de l'Armée de Dumbledore. L'introduction de Dolores Ombrage, avec son régime tyrannique à Poudlard, ajoute une nouvelle dimension de conflit. Le développement des personnages est plus profond, révélant leurs vulnérabilités et leurs forces. Cependant, le ton plus sombre et l'augmentation de la longueur du livre peuvent rendre la lecture plus exigeante. Malgré cela, "Harry Potter et l'Ordre du Phénix" est un chapitre crucial et captivant de la saga, offrant des moments de tension intense et des révélations importantes.</div>
 
 
-                    <h3>Propriétaire</h3>
+                    <h3 class="subtitle">Propriétaire</h3>
 
-                    <span class="detail_owner">Admin</span>
+                    <a href="" class="book_owner">
+                        <img src="img/HK.jpg" class="detail_profile_picture" alt="image de profil">
+                        <span>Admin</span>
+                    </a>
                 </div>
 
                 <a href="" class="button_type_1" > Envoyer un message </a>
